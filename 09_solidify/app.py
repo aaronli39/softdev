3f89f175cd502b925e6be9858c4e1c68b68328aa
Aaron Li <aaronli39@gmail.com>
2018-09-21 23:55:37 -0400

    did hw!

--- 09_solidify/app.py
+++ 09_solidify/app.py
@@ -8,23 +8,25 @@ import csv
 # instantiate instance of Flask class
 app = Flask(__name__)
 
-collection = [0, 1, 1, 2, 3, 5, 8]
+csvDict = {} # dictionary with percentages as keys
+dict = {} # REAL dictionary with occupation as keys
+keys = [] # stores REAL dictionary keys
+list = [] # list to store a list of percentages(this is for the weighted random function)
+rows = 0 # how many rows of csv files
 
-# dictionary that will hold floating percentages as keys, and key-values are
-# the occupations
-csvDict = {}
-# list to store a list of percentages(this is for the weighted random function)
-list = []
 
 # with statement means less errors(automatically closes file)
 with open("data/occupations.csv","r") as csvF:
     csvR = csv.reader(csvF) # reads the csv file
     # iterate through the csv file
     for line in csvR:
-        # as long as its not the first and last lines(which has no important info)
-        # add the percentages to the dictionary as a key, and add the appropriate
-        # key value(occupation)
+        # add the percentages to the dictionary as a key
         if line[0] != "Job Class" and line[0] != "Total":
+            key = line[0]
+            rows += 1
+            dict.setdefault(key, [])
+            dict[key].extend([line[1], line[2]])
+            keys.append(key)
             csvDict[float(line[1])] = line[0]
             list.append(float(line[1])) # also add the percentage to the list
 
@@ -46,28 +48,27 @@ def weight(list):
     for i in list:
         temp += i
         if temp >= rand:
-            print(csvDict[list[index]])
+            return(csvDict[list[index]])
             break
         else: # otherwise increase the index to the next-to-smallest percentage
             index += 1
 
+@app.route("/")
+def welcome():
+    return render_template('home.html')
 
 
-            
 # route 1: homepage
-@app.route("/my_foist_template")
-def herro():
-    text = '''
-    <h1 style='text-align: center;'>Hello Peeps!</h1>
-    <br><br><form action="/static/index.html">
-        <center><input type="submit" value="Click for Surprise!"/></center>
-    </form>'''
-    print(weight(list))
-    return render_template('index.html',
-                               d=collection,
-                               i=0,
-                               name="fooooo")
-
 @app.route("/occupations")
-def occu():
-    return weight(list)
+def herro():
+
+    return render_template('occupations.html',
+                               name = "Occupations Data",
+                               occu = weight(list),
+                               occuList = keys,
+                               dict = dict
+                               )
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
