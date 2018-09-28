37941d98f8ceab3da76000f047d0d77700464b51
aaronli39 <aaronli39@gmail.com>
2018-09-28 12:30:30 -0400

    added heading, can accept POST and GET

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -1,3 +1,8 @@
+# Xiaojie(Aaron) Li
+# SoftDev1 pd6
+# K13 -- Echo Echo Echo
+# 2018-09-27
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -8,11 +13,14 @@ def hello():
     print(app)
     return render_template("test.html")
 
-@app.route("/greetings", methods = ["POST"])
+@app.route("/greetings", methods = ["POST", "GET"])
 def greet():
     print(request.method)
     print(request.form)
-    name = request.form["user"]
+    if request.method == "GET":
+        name = request.args["user"]
+    else:
+        name = request.form["user"]
     method = request.method
     return render_template("greet.html", name = name, method = method)
 
