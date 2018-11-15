59f9f1eb3d4fd30f45121ac2e5a95f79d8a89ee8
Aaron Li <aaronli39@gmail.com>
2018-11-15 16:27:41 -0500

    did hw

--- 26_/app.py
+++ 26_/app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template, request
+
+import json, urllib.request, os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home() :
-    return;
+def home():
+    return render_template("index.html")
+
+@app.route("/world")
+def world():
+    url = urllib.request.urlopen("http://api.population.io:80/1.0/countries")
+    data = json.loads(url.read().decode())
+    print(data["countries"])
+    print("\n\n------------------------------------")
+    print(data["countries"][2])
+    countries = data["countries"]
+    # for i in range(0, 3):
+    #     countries.remove(countries.index("Less developed"))
+    # print(countries)
+    return render_template('api_1.html', countries = data["countries"])
 
 if __name__ == "__main__":
     app.run(debug = True)
