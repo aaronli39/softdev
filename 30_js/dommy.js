c78b72a66b5a2e2e64d8dee722051f2ba549d536
th0mazzz <thomas@Thomaz-ZenBook.localdomain>
2018-12-20 18:43:40 -0500

    finished hw

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -1,34 +1,49 @@
-// VALUE-ADDED KEY TO SUCCESS!!!!!!!!!!!
+/*
+Team weWantJS
+Xiaojie Li, Ricky Lin
+SoftDev1 pd6
+K30 -- Sequential Progression III: Season of the Witch
+2018-12-20
+*/
 
+
+// VALUE-ADDED KEY TO SUCCESS!!!!!!!!!!!
 var changeHeading = function(e) {
     var h = document.getElementById("h");
-    h.innerHTML = e.target.innerhtml;
+    h.innerHTML = this.innerHTML;
+    console.log(this.innerHTML);
 };
 
 var removeItem = function(e) {
-    e.target.remove();
+    this.remove();
 };
 
 var lis = document.getElementsByTagName("li");
 
 for (var i = 0; i < lis.length; i++) {
     lis[i].addEventListener("mouseover", changeHeading);
-    lis[i].addEventListener("mouseout", ?);
-    lis[i].addEventListener("click", ?);
+    lis[i].addEventListener("mouseout", function() {
+        var h = document.getElementById("h");
+        h.innerHTML = "Hello World!";
+    });
+    lis[i].addEventListener("click", removeItem);
 };
 
 var addItem = function(e) {
-    var list = ?
+    var list = document.getElementById("thelist");
     var item = document.createElement("li");
     item.innerHTML = "WORD";
-    ??
-    ..
-    ??
-    list.??(item);
+    list.appendChild(item);
+    item.addEventListener("mouseover", changeHeading);
+    item.addEventListener("mouseout", function() {
+        var h = document.getElementById("h");
+        h.innerHTML = "Hello World!";
+    });
+    item.addEventListener("click", removeItem);
 };
 
 var button = document.getElementById("b");
-button.addEventListener("clock", addItem);
+button.addEventListener("click", addItem);
 
 var fib = function(n) {
     if (n < 2) {
@@ -38,17 +53,21 @@ var fib = function(n) {
     }
 };
 
+var counter = 1;
 var addFib = function(e) {
     console.log(e);
-    ??
-    ??
+    var list = document.getElementById("fiblist");
+    var item = document.createElement("li");
+    item.innerHTML = "" + fib(counter);
+    counter += 1;
+    list.appendChild(item);
 };
 
-var addFib2 = function(e) {
-    console.log(e);
-    ??
-    .. see QAF: re dynamic programming
-};
+// var addFib2 = function(e) {
+//     console.log(e);
+//     ??
+//     .. see QAF: re dynamic programming
+// };
 
 var fb = document.getElementById("fb");
 fb.addEventListener("click", addFib);
